eebec0b87af9009d06ac5541de908c655b7b14a8
Jarom Loveridge <jaroml@gradecam.com>
2018-04-15 14:41:40 -0600

    Update build script to remove dist folder prior to compilation.

--- package-scripts.js
+++ package-scripts.js
@@ -2,7 +2,7 @@ const npsUtils = require('nps-utils');
 
 module.exports = {
   scripts: {
-    build: 'tsc',
+    build: 'rimraf dist && tsc',
     default: 'mocha',
     lint: 'tslint --project .',
     prepublishOnly: npsUtils.concurrent.nps('lint', 'test', 'build'),
